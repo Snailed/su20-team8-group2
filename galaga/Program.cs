5e6ed710c05931f1977a8e3c88c4169be651b967
Snailed <rasmus.hag.lovstad@gmail.com>
2020-02-25 15:29:31 +0100

    Started game in main method

---
+++ galaga/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace galaga
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Game game = new Game();
+            game.GameLoop();
+        }
+    }
+}
